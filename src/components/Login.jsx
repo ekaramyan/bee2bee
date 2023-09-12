e26ab355cf513e7de0cb0669ccb349542acc2da8
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-09-13 00:36:57 +0400

    started login/register forms

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import background from '@/assets/img/auth_bg.svg'
+import { Typography } from '@mui/material'
+
+export default function Login() {
+	return (
+		<div style={{ background: `url(${background.src})` }}>
+			<Typography variant=''>Members Login</Typography>
+			<>
+				<form action=''></form>
+			</>
+		</div>
+	)
+}
