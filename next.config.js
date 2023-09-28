d0d2443178fd99e9569d92fe894aff4874ab7be6
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-09-28 23:43:58 +0400

    updated config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+require('dotenv').config()
+
+const nextConfig = {
+	output: 'standalone',
+	// images: {
+	// 	domains: [process.env.APP_DOMAIN],
+	// },
+	env: {
+		API_URL: process.env.API_URL,
+		API_TOKEN: process.env.API_TOKEN,
+		// APP_DOMAIN: process.env.APP_DOMAIN,
+	},
+}
 
 module.exports = nextConfig
