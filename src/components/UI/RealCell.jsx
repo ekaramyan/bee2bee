96877c30edcf7d8f59559c7bd9c879ba05dde711
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-10-11 00:07:13 +0400

    build error fix

--- src/components/UI/RealCell.jsx
+++ src/components/UI/RealCell.jsx
@@ -24,6 +24,7 @@ export default function RealCell({ level, data }) {
 				>
 					{data?.map((item, index) => (
 						<div
+							key={index}
 							style={{
 								background: `url(${cell.src}) no-repeat center / contain`,
 								width: 100,
