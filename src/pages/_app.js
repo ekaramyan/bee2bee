796359219f48bd85eacfe187998357e0a4bb689f
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-11-03 05:01:31 +0400

    added meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,6 +20,33 @@ function MyApp({ Component, pageProps }) {
 					async
 					defer
 				></script>
+				<meta
+					name='description'
+					content='Bee2Bee: Opportunities for everyone. Connect, collaborate, and grow together in the business world.'
+				/>
+				<meta
+					name='keywords'
+					content='business, collaboration, networking, growth, opportunities, partners, bee2bee'
+				/>
+
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<meta charSet='utf-8' />
+
+				<meta property='og:title' content='Bee2Bee' />
+				<meta
+					property='og:description'
+					content='Opportunities for everyone. Connect, collaborate, and grow together in the business world.'
+				/>
+				<meta property='og:image' content='/path_to_your_og_image.jpg' />
+				<meta property='og:url' content='https://bee2bee.online' />
+
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta name='twitter:title' content='Bee2Bee' />
+				<meta
+					name='twitter:description'
+					content='Opportunities for everyone. Connect, collaborate, and grow together in the business world.'
+				/>
+				<meta name='twitter:image' content='/path_to_your_twitter_image.jpg' />
 			</Head>
 			<ThemeProvider theme={theme}>
 				<Provider store={store}>
