336596cc9bc781f552abf29e72e9073ae7b390f3
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-11-03 04:55:19 +0400

    added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import Head from 'next/head'
 import theme from '../Theme'
 import IndexWrapper from '@/components/UI/index_wrapper'
 import store from '@/store'
+import favicon from '@/assets/img/favicon.webp'
 
 function MyApp({ Component, pageProps }) {
 	const captchaKey = process.env.CAPTCHA_KEY
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }) {
 		<>
 			<Head>
 				<title>Bee2Bee</title>
+				<link rel='icon' href={favicon.src} />
 				<script
 					src='https://www.google.com/recaptcha/api.js'
 					data-sitekey={captchaKey}
