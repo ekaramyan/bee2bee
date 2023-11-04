a3cdac17b71371544b4b9402c9e8320b63d839ea
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-11-05 01:52:19 +0400

    hided reset on phones

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, TextField } from '@mui/material'
+import { Box, Grid, TextField, useMediaQuery } from '@mui/material'
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined'
 import { useState } from 'react'
 import dynamic from 'next/dynamic'
@@ -13,70 +13,78 @@ export default function ChangePassForm() {
 		event.preventDefault()
 		getEmail(event.target.email.value)
 	}
+	const isMobile = useMediaQuery('@media(max-width: 420px)')
 
 	return (
 		<>
-			<Box
-				style={{
-					width: '100%',
-					height: '100%',
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					justifyContent: 'center',
-					gap: 20,
-					padding: '20px 10px',
-				}}
-			>
-				<form
-					onSubmit={handleSubmit}
+			{!isMobile ? ( //delete it when issue is fixed
+				<Box
 					style={{
+						width: '100%',
+						height: '100%',
 						display: 'flex',
-						flex: '3',
 						flexDirection: 'column',
 						alignItems: 'center',
 						justifyContent: 'center',
 						gap: 20,
+						padding: '20px 10px',
 					}}
 				>
-					<Box item xs={12} style={{ display: 'flex', gap: 20 }}>
-						<TextField
-							label={
-								<Box
-									style={{
-										display: 'flex',
-										alignItems: 'center',
-										gap: 5,
-									}}
-								>
-									<EmailOutlinedIcon sx={{ color: iconColors }} />
-									Email
-								</Box>
-							}
-							variant='standard'
-							fullWidth
-							type='email'
-							name='email'
-							onFocus={() => setIconColors('action')}
-							onBlur={() => setIconColors('#8C7F77')}
-						/>
-					</Box>
-
-					<Grid
-						item
+					<form
+						onSubmit={handleSubmit}
 						style={{
 							display: 'flex',
+							flex: '3',
 							flexDirection: 'column',
-							width: '100%',
-							alignSelf: 'end',
+							alignItems: 'center',
+							justifyContent: 'center',
+							gap: 20,
 						}}
 					>
-						<AuthButton type='submit'>Send email</AuthButton>
-					</Grid>
-				</form>
-				{error && <div>{error}</div>}
-				{success && <div>Successfully changed password!</div>}
-			</Box>
+						<Box item xs={12} style={{ display: 'flex', gap: 20 }}>
+							<TextField
+								label={
+									<Box
+										style={{
+											display: 'flex',
+											alignItems: 'center',
+											gap: 5,
+										}}
+									>
+										<EmailOutlinedIcon sx={{ color: iconColors }} />
+										Email
+									</Box>
+								}
+								variant='standard'
+								fullWidth
+								type='email'
+								name='email'
+								onFocus={() => setIconColors('action')}
+								onBlur={() => setIconColors('#8C7F77')}
+							/>
+						</Box>
+
+						<Grid
+							item
+							style={{
+								display: 'flex',
+								flexDirection: 'column',
+								width: '100%',
+								alignSelf: 'end',
+							}}
+						>
+							<AuthButton type='submit'>Send email</AuthButton>
+						</Grid>
+					</form>
+					{error && <div>{error}</div>}
+					{success && <div>Successfully sent email for password reset!</div>}
+				</Box>
+			) : (
+				<>
+					This function is currently unavailable on mobile devices, we are
+					actively fixing it, please try it on your PC
+				</>
+			)}
 		</>
 	)
 }
