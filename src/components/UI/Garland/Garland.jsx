8dec9581c5a3f64f654ee0307cb6ae79d4e2b3d2
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-12-31 19:24:46 +0400

    balls import to jsx

--- src/components/UI/Garland/Garland.jsx
+++ src/components/UI/Garland/Garland.jsx
@@ -1,15 +1,36 @@
-const Ball = ({ number, note, bounce }) => {
+import b_decor from '../../../styles/balls/b-head-decor_newyear.webp'
+import bBallN1 from '../../../styles/balls/b-ball_n1.webp'
+import bBallN2 from '../../../styles/balls/b-ball_n2.webp'
+import bBallN3 from '../../../styles/balls/b-ball_n3.webp'
+import bBallN4 from '../../../styles/balls/b-ball_n4.webp'
+import bBallN5 from '../../../styles/balls/b-ball_n5.webp'
+import bBallN6 from '../../../styles/balls/b-ball_n6.webp'
+import bBallN7 from '../../../styles/balls/b-ball_n7.webp'
+import bBallN8 from '../../../styles/balls/b-ball_n8.webp'
+import bBallN9 from '../../../styles/balls/b-ball_n9.webp'
+import bBallI1 from '../../../styles/balls/b-ball_i1.webp'
+import bBallI2 from '../../../styles/balls/b-ball_i2.webp'
+import bBallI3 from '../../../styles/balls/b-ball_i3.webp'
+import bBallI4 from '../../../styles/balls/b-ball_i4.webp'
+import bBallI5 from '../../../styles/balls/b-ball_i5.webp'
+import bBallI6 from '../../../styles/balls/b-ball_i6.webp'
+
+const Ball = ({ number, note, bounce, backgroundImage }) => {
+	const ballStyle = {
+		backgroundImage: `url(${backgroundImage})`,
+		backgroundRepeat: 'no-repeat',
+	}
+
 	return (
 		<div
 			className={`b-ball b-ball_n${number} ${bounce ? 'b-ball_bounce' : ''}`}
 			data-note={note}
 		>
 			<div className='b-ball__right'></div>
-			<div className='b-ball__i'></div>
+			<div className='b-ball__i' style={ballStyle}></div>
 		</div>
 	)
 }
-import b_decor from '../../../styles/balls/b-head-decor_newyear.webp'
 
 const NewYearPage = ({ style }) => {
 	const decorBg = {
@@ -25,8 +46,32 @@ const NewYearPage = ({ style }) => {
 		zIndex: 0,
 	}
 	const createBalls = (count, startNote, bounce) => {
+		const ballImages = [
+			bBallN1,
+			bBallN2,
+			bBallN3,
+			bBallN4,
+			bBallN5,
+			bBallN6,
+			bBallN7,
+			bBallN8,
+			bBallN9,
+			bBallI1,
+			bBallI2,
+			bBallI3,
+			bBallI4,
+			bBallI5,
+			bBallI6,
+		]
+
 		return Array.from({ length: count }, (_, i) => (
-			<Ball key={i} number={i + 1} note={startNote + i} bounce={bounce} />
+			<Ball
+				key={i}
+				number={i + 1}
+				note={startNote + i}
+				bounce={bounce}
+				backgroundImage={ballImages[i]}
+			/>
 		))
 	}
 
