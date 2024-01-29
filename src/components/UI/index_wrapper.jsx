29b46ec3218c4832d17b04f9b5850dfef7042af6
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2024-01-30 02:19:48 +0400

    turned off snow

--- src/components/UI/index_wrapper.jsx
+++ src/components/UI/index_wrapper.jsx
@@ -1,36 +1,36 @@
 import { Container, useMediaQuery, Box, styled } from '@mui/material'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { useSelector } from 'react-redux'
 import dynamic from 'next/dynamic'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import AuthButton from './AuthButton'
-import Snowfall from 'react-snowfall'
+// import Snowfall from 'react-snowfall'
 
 const Login = dynamic(() => import('../Login'))
 const Register = dynamic(() => import('../Register'))
 const MyCells = dynamic(() => import('../../components/MyCells'))
 const RealCells = dynamic(() => import('../../components/RealCells'))
 const SideModal = dynamic(() => import('./SideModal'))
-import snowflake1 from '../../assets/img/snowflake1.png'
-import snowflake2 from '../../assets/img/snowflake2.png'
-import snowflake3 from '../../assets/img/snowflake3.png'
-import snowflake4 from '../../assets/img/snowflake4.png'
+// import snowflake1 from '../../assets/img/snowflake1.png'
+// import snowflake2 from '../../assets/img/snowflake2.png'
+// import snowflake3 from '../../assets/img/snowflake3.png'
+// import snowflake4 from '../../assets/img/snowflake4.png'
 import background from '../../assets/img/background.webp'
 
-const SnowfallContainer = styled('div')`
-	position: fixed;
-	top: 0;
-	left: 0;
-	bottom: 0;
-	right: 0;
-	z-index: 0;
-	overflow: hidden;
-	background: #00000040;
-	width: 100%;
-	height: 100vh;
-`
+// const SnowfallContainer = styled('div')`
+// 	position: fixed;
+// 	top: 0;
+// 	left: 0;
+// 	bottom: 0;
+// 	right: 0;
+// 	z-index: 0;
+// 	overflow: hidden;
+// 	background: #00000040;
+// 	width: 100%;
+// 	height: 100vh;
+// `
 
 const IndexWrapper = ({ children, ...props }) => {
 	const [isLoginOpen, setIsLoginOpen] = useState(false)
@@ -40,31 +40,31 @@ const IndexWrapper = ({ children, ...props }) => {
 	const isMobile = useMediaQuery('@media(max-width: 1300px)')
 	const isLow = useMediaQuery('@media(min-height: 880px)')
 
-	const [snowflakeImages, setSnowflakeImages] = useState([])
+	// const [snowflakeImages, setSnowflakeImages] = useState([])
 
-	useEffect(() => {
-		const loadSnowflakeImages = async () => {
-			const loadImage = async src => {
-				const image = new Image()
-				image.src = src
-				await new Promise(resolve => {
-					image.onload = resolve
-				})
-				return image
-			}
+	// useEffect(() => {
+	// 	const loadSnowflakeImages = async () => {
+	// 		const loadImage = async src => {
+	// 			const image = new Image()
+	// 			image.src = src
+	// 			await new Promise(resolve => {
+	// 				image.onload = resolve
+	// 			})
+	// 			return image
+	// 		}
 
-			const images = await Promise.all([
-				loadImage(snowflake1.src),
-				loadImage(snowflake2.src),
-				loadImage(snowflake3.src),
-				loadImage(snowflake4.src),
-			])
+	// 		const images = await Promise.all([
+	// 			loadImage(snowflake1.src),
+	// 			loadImage(snowflake2.src),
+	// 			loadImage(snowflake3.src),
+	// 			loadImage(snowflake4.src),
+	// 		])
 
-			setSnowflakeImages(images)
-		}
+	// 		setSnowflakeImages(images)
+	// 	}
 
-		loadSnowflakeImages()
-	}, [])
+	// 	loadSnowflakeImages()
+	// }, [])
 
 	const toggleLogin = () => {
 		setIsLoginOpen(!isLoginOpen)
@@ -72,7 +72,7 @@ const IndexWrapper = ({ children, ...props }) => {
 	const toggleRegister = () => {
 		setIsRegisterOpen(!isRegisterOpen)
 	}
-	console.log(snowflakeImages, 'snow')
+
 	return (
 		<>
 			<div
@@ -89,13 +89,13 @@ const IndexWrapper = ({ children, ...props }) => {
 				}}
 				className='ScrollbarWhite'
 			>
-				<SnowfallContainer>
+				{/* <SnowfallContainer>
 					<Snowfall
 						snowflakeCount={400}
 						radius={[5.5, 20.0]}
 						images={snowflakeImages}
 					/>
-				</SnowfallContainer>
+				</SnowfallContainer> */}
 
 				<Container
 					className='ScrollbarDefault'
