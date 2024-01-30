181e58fe894dadd533bf61f438226e294f8de13b
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2024-01-30 16:01:05 +0400

    deleted erroneous limit

--- src/pages/cells/[cellLevelId]/index.jsx
+++ src/pages/cells/[cellLevelId]/index.jsx
@@ -23,7 +23,7 @@ export async function getServerSideProps(context) {
 	const id = context.params.cellLevelId
 	const token = req.cookies.access_token
 	const apiUrl = process.env.API_URL
-	const url = `${apiUrl}/cells/all/list?level_id=${id}&limit=1`
+	const url = `${apiUrl}/cells/all/list?level_id=${id}`
 	const joinListUrl = `${apiUrl}/cells/join/list?level_id=${id}&limit=1`
 	const levelUrl = `${apiUrl}/cell-levels/${id}`
 
