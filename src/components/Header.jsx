4b2f95feda3856e266e8e522e23c56fd78086e1b
vanyavvorobevv <100296048+vanyavorobev@users.noreply.github.com>
2025-01-20 18:48:19 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -68,7 +68,7 @@ export default function Header({ loggedIn }) {
 						}}
 					>
 						<Box style={{ display: 'flex', alignItems: 'center' }}>
-							<CelebrationMessage/>
+							{/* <CelebrationMessage/> */}
 							{/* <CelebrationTimer /> */}
 						</Box>
 						{loggedIn ? (
